7f568a6456a18a496c9bf68105c68cfab6dab8d4
Bogdan Bercea <bogdanbercea6@gmail.com>
2022-08-02 16:23:16 +0300

    Add authentication page
    Squashed commit of the following:
    
    commit 9b1be1fdaa541dce2ae1f63d88bb4e16e0f75fc8
    Author: Bogdan Bercea <bogdanbercea6@gmail.com>
    Date:   Tue Aug 2 16:20:55 2022 +0300
    
        Finish sign in page
    
    commit 50bd9030418b4f1643d1aa8f9804d454e2a444a5
    Author: Bogdan Bercea <bogdanbercea6@gmail.com>
    Date:   Tue Aug 2 15:42:56 2022 +0300
    
        Add sign up form
    
    commit 63aa49f0c26d3cb0ed33ac884e49440668aaa81c
    Author: Bogdan Bercea <bogdanbercea6@gmail.com>
    Date:   Mon Aug 1 21:00:22 2022 +0300
    
        Add Google authentication using Firebase
    
    commit 02626862cd6d05c21dc09b34ca68c98a91f5b063
    Author: Bogdan Bercea <bogdanbercea6@gmail.com>
    Date:   Mon Aug 1 10:21:58 2022 +0300
    
        Add directory component and category-item component
    
    commit 59b29163fd381c0e72e377f5ad025623870caef7
    Author: Bogdan Bercea <bogdanbercea6@gmail.com>
    Date:   Sun Jul 31 20:26:36 2022 +0300
    
        Setup basic project structure

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,77 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCotg4_6s20Agt9NeJcP2yWYsIBf3bA3FM",
+  authDomain: "crwn-clothing-db-79305.firebaseapp.com",
+  projectId: "crwn-clothing-db-79305",
+  storageBucket: "crwn-clothing-db-79305.appspot.com",
+  messagingSenderId: "540055102191",
+  appId: "1:540055102191:web:1cc910412e9afb8077311a"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) => {
+  if (!userAuth) {
+    return;
+  }
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInfo
+      });
+    } catch (error) {
+      console.log('An error occured while creating a new user.', error.message);
+    }
+  }
+
+  return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) {
+    return;
+  }
+
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) {
+    return;
+  }
+
+  return signInWithEmailAndPassword(auth, email, password);
+}
