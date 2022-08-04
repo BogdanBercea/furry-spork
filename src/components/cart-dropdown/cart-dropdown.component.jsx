aa562e6d5f1f92dfd21c2d80068699872679363a
Bogdan Bercea <bogdanbercea6@gmail.com>
2022-08-04 21:06:26 +0300

    Finish checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component';
@@ -9,6 +10,11 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('checkout')
+  }
 
   return (
     <div className='cart-dropdown-container'>
@@ -18,7 +24,7 @@ const CartDropdown = () => {
           <CartItem key={cartItem.id} cartItem={cartItem} />
         ))
       }
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,7 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
-  console.log(cartItems);
   const existingCartItem = cartItems.find((product) => product.id === productToAdd.id);
 
   if (existingCartItem) {
@@ -15,21 +14,61 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 }
 
+const removeCartItem = (cartItems, productToRemove) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToRemove.id);
+
+  if (!existingCartItem) {
+    return;
+  }
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id);
+  }
+
+  return cartItems.map((cartItem) => (
+    cartItem.id === productToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  ));
+}
+
+const clearCartItem = (cartItems, productToRemove) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToRemove.id);
+
+  if (!existingCartItem) {
+    return;
+  }
+
+  return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id);
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => { },
   cartItems: [],
   addItemToCart: () => { },
+  removeItemFromCart: () => { },
+  clearItemFromCart: () => { },
   cartCount: 0,
+  cartTotal: 0
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
+  }
+
+  const removeItemFromCart = (productToRemove) => {
+    setCartItems(removeCartItem(cartItems, productToRemove))
+  }
+
+  const clearItemFromCart = (productToRemove) => {
+    setCartItems(clearCartItem(cartItems, productToRemove));
   }
 
   useEffect(() => {
@@ -37,7 +76,12 @@ export const CartProvider = ({ children }) => {
     setCartCount(newCartCount);
   }, [cartItems]);
 
-  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount }
+  useEffect(() => {
+    const newTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
+    setCartTotal(newTotal);
+  }, [cartItems]);
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, removeItemFromCart, clearItemFromCart, cartCount, cartTotal }
 
   return (
     <CartContext.Provider value={value}>{children}</CartContext.Provider>
