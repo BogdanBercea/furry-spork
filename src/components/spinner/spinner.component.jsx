a1c249629617e2ff4e5abb5df2e82490352af0d9
Bogdan Bercea <bogdanbercea6@gmail.com>
2022-08-14 09:14:54 +0300

    Add spinner component and use it in category and categories-preview components

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,11 @@
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.stiles";
+
+const Spinner = () => {
+  return (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  )
+}
+
+export default Spinner;
