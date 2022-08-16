44fcdea163641d4e42d482f0c405077fb01d0f8d
Bogdan Bercea <bogdanbercea6@gmail.com>
2022-08-16 20:42:08 +0300

    Complete stripe payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -14,16 +14,43 @@ const PaymentForm = () => {
       return;
     }
 
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ amount: 10000 })
+    }).then(res => res.json());
 
+    const { paymentIntent: { client_secret } } = response;
+
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Bercea Bogdan'
+        }
+      }
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Successfull');
+      }
+    }
   }
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
       </FormContainer>
-      <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
     </PaymentFormContainer>
   );
 }
